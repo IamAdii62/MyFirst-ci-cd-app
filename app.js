eb6c136b0fa79cdc4e9e440040fda42a30626c87
Aditi Kumari <aditikumari4656@gmail.com>
2026-04-08 22:14:03 +0530

    trigger pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD pipeline 🚀");
+  res.send("Hello Aditi CI/CD 🔥🔥");
 });
 
 app.listen(3000, () => {
