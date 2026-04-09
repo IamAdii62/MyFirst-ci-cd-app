460dcfb575d07680da0d4df9671f9a70bfb024fd
Aditi Kumari <aditikumari4656@gmail.com>
2026-04-09 17:00:42 +0530

    added ci cd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from CI/CD App 🚀");
+  res.send("CI/CD SUCCESS 🚀");
 });
 
 app.listen(3000, () => {
